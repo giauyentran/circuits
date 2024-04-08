381a03b57828457a21ce2fd8fbdbe5cf7c62f908
giauyentran <giauyen03@gmail.com>
2024-04-08 01:53:10 -0400

    update styling

--- lab5/exp1.m
+++ lab5/exp1.m
@@ -1,5 +1,15 @@
+% Run this section before running any other section
 clear; clf;
 
+% Colors for plots
+taupe_gray = [156 151 154]./255;
+bittersweet = [255 108 140]./255;
+light_sea_green = [16 161 156]./255;
+black = [0 0 0]./255;
+white = [255 255 255]./255;
+%% 
+
+clf;
 load data/Lab5Equal.mat
 
 % include a single plot showing I1, I2, I1 − I2, and I1 + I2, as a function 
@@ -9,29 +19,35 @@ load data/Lab5Equal.mat
 % V2.)
 
 % I1
-hold on
-plot(Ch1Vdm11, Ch2I111, 'r.')
-plot(Ch1Vdm12, Ch2I112, 'g.')
-plot(Ch1Vdm13, Ch2I113, 'b.')
+hold on; grid minor
+plot(Ch1Vdm13, Ch2I113, '.', Color=black)
+plot(Ch1Vdm12, Ch2I112, '.', Color=light_sea_green)
+plot(Ch1Vdm11, Ch2I111, '.', Color=bittersweet)
 
 % I2
-plot(Ch1Vdm21, Ch2I121, 'r.')
-plot(Ch1Vdm22, Ch2I122, 'g.')
-plot(Ch1Vdm23, Ch2I123, 'b.')
+plot(Ch1Vdm23, Ch2I123, '.', Color=black)
+plot(Ch1Vdm22, Ch2I122, '.', Color=light_sea_green)
+plot(Ch1Vdm21, Ch2I121, '.', Color=bittersweet)
 
 % I1 - I2
-plot(Ch1Vdm21, Ch2I111-Ch2I121, 'r.')
-plot(Ch1Vdm22, Ch2I112-Ch2I122, 'g.')
-plot(Ch1Vdm23, Ch2I113-Ch2I123, 'b.')
+plot(Ch1Vdm23, Ch2I113-Ch2I123, '.', Color=black)
+plot(Ch1Vdm22, Ch2I112-Ch2I122, '.', Color=light_sea_green)
+plot(Ch1Vdm21, Ch2I111-Ch2I121, '.', Color=bittersweet)
 
 
 % I1 + I2
-plot(Ch1Vdm21, Ch2I111+Ch2I121, 'r.')
-plot(Ch1Vdm22, Ch2I112+Ch2I122, 'g.')
-plot(Ch1Vdm23, Ch2I113+Ch2I123, 'b.')
+plot(Ch1Vdm23, Ch2I113+Ch2I123, '.', Color=black)
+plot(Ch1Vdm22, Ch2I112+Ch2I122, '.', Color=light_sea_green)
+plot(Ch1Vdm21, Ch2I111+Ch2I121, '.', Color=bittersweet)
+
+text(0,1.3e-7,'I_1+I_2')
+text(-0.3,0.1e-7,'I_1')
+text(0.3,0.1e-7,'I_2')
+text(-0.3,-1.05e-7,'I_1-I_2')
 
 title("Differential Pair Characteristics At Threshold (V_b = 0.5V)")
-legend("4.5V", '3.5V', '2.5V', Location='southeast')
+lgd = legend("2.5V", '3.5V', '4.5V', Location='southeast');
+title(lgd,'V_2','FontSize',10)
 xlabel('Differential-Mode Input Voltage (V)')
 ylabel('Current (A)')
 %% 
@@ -39,18 +55,18 @@ ylabel('Current (A)')
 % Also include a plot showing the common-source node voltage, V , as a 
 % function of V1 − V2 for all three values of V2. How does the value of V 
 % change as V1 goes from below V2 to above it?
-
-clear; clf;
-
+clf;
 load data/exp1_WI.mat
 
-hold on
-plot(Ch1Vdm31, Ch2V31, '.')
-plot(Ch1Vdm32, Ch2V32, '.')
-plot(Ch1Vdm33, Ch2V33, '.')
+hold on; grid minor
+plot(Ch1Vdm33, Ch2V33, '.', Color=black)
+plot(Ch1Vdm32, Ch2V32, '.', Color=light_sea_green)
+plot(Ch1Vdm31(21:121), Ch2V31(21:121), '.', Color=bittersweet)
 
-legend('4.5 V', '3.5 V', '2.5 V')
-title('Common-Source Node Voltage Below Threshold (V_b = 0.5 V)')
+ylim([1 4])
+lgd = legend('2.5 V', '3.5 V', '4.5 V', Location = 'northwest');
+title(lgd,'V_2','FontSize',10);
+title('Common-Source Node Voltage At Threshold (V_b = 0.5 V)')
 xlabel('Differential-Mode Input Voltage (V)')
 ylabel('Common-Source Node Voltage (V)')
 %% Plot 1
@@ -59,7 +75,7 @@ ylabel('Common-Source Node Voltage (V)')
 % the plot of I1 − I2 as a function of V1 − V2 around the region where 
 % V1 ≈ V2 (i.e., where V1 − V2 ≈ 0 V)
 
-clear; clf;
+clf
 load data/Lab5Equal.mat
 
 [f1,l1,m1,b1,n1] = linefit(Ch1Vdm21, Ch2I111-Ch2I121, 5e-3);
@@ -71,20 +87,20 @@ Gdm_35 = m2;
 Gdm_45 = m1;
 
 
-hold on
+hold on; grid minor
 % Plot Data
-plot(Ch1Vdm23, Ch2I113-Ch2I123, 'b.')
-plot(Ch1Vdm22, Ch2I112-Ch2I122, 'g.')
-plot(Ch1Vdm21, Ch2I111-Ch2I121, 'r.')
+plot(Ch1Vdm23, Ch2I113-Ch2I123, '.', Color=black)
+plot(Ch1Vdm22, Ch2I112-Ch2I122, '.', Color=light_sea_green)
+plot(Ch1Vdm21, Ch2I111-Ch2I121, '.', Color=bittersweet)
 % Plot Fit
-plot(Ch1Vdm23, (Ch1Vdm23)*m3+b3, 'b-')
-plot(Ch1Vdm22, (Ch1Vdm22)*m2+b2, 'g-')
-plot(Ch1Vdm21, (Ch1Vdm21)*m1+b1, 'r-')
+plot(Ch1Vdm23, (Ch1Vdm23)*m3+b3, '-', Color=black)
+plot(Ch1Vdm22, (Ch1Vdm22)*m2+b2, '-', Color=light_sea_green)
+plot(Ch1Vdm21, (Ch1Vdm21)*m1+b1, '-', Color=bittersweet)
 
 ylim([-1.5e-7 1.5e-7])
 
-lgd = legend('2.5 V', '3.5 V', '4.5 V');
-title(lgd,'V_2','FontSize',10)
+legend('V_2=2.5 V', 'V_2=3.5 V', 'V_2=4.5 V', 'G_{dm}=1.4512e-06 mhos', ...
+    'G_{dm}=1.4764e-06 mhos', 'G_{dm}=1.5976e-06 mhos', Location="southeast");
 xlabel("Differential-Mode Input Voltage (V)")
 ylabel("I1-I2 (A)")
 
@@ -97,46 +113,92 @@ ylabel('I_1-I_2 Current Difference (A)')
 % Make plot similar to the ones that you made for the lower bias current. 
 % How does the behavior of the circuit change as the bias current changes 
 % from weak or moderate inversion to strong inversion?
-clear; clf;
+clf;
 load data/exp1_SI.mat
 
 figure(1)
-hold on 
+hold on; grid minor
+
 % I1
-hold on
-plot(Ch1Vdm11, Ch2I111, 'r.')
-plot(Ch1Vdm12, Ch2I112, 'g.')
-plot(Ch1Vdm13, Ch2I113, 'b.')
+plot(Ch1Vdm13, Ch2I113, '.', Color=black)
+plot(Ch1Vdm12, Ch2I112, '.', Color=light_sea_green)
+plot(Ch1Vdm11, Ch2I111, '.', Color=bittersweet)
 
 % I2
-plot(Ch1Vdm21, Ch2I121, 'r.')
-plot(Ch1Vdm22, Ch2I122, 'g.')
-plot(Ch1Vdm23, Ch2I123, 'b.')
+plot(Ch1Vdm23, Ch2I123, '.', Color=black)
+plot(Ch1Vdm22, Ch2I122, '.', Color=light_sea_green)
+plot(Ch1Vdm21, Ch2I121, '.', Color=bittersweet)
 
 % I1 - I2
-plot(Ch1Vdm21, Ch2I111-Ch2I121, 'r.')
-plot(Ch1Vdm22, Ch2I112-Ch2I122, 'g.')
-plot(Ch1Vdm23, Ch2I113-Ch2I123, 'b.')
+plot(Ch1Vdm23, Ch2I113-Ch2I123, '.', Color=black)
+plot(Ch1Vdm22, Ch2I112-Ch2I122, '.', Color=light_sea_green)
+plot(Ch1Vdm21, Ch2I111-Ch2I121, '.', Color=bittersweet)
 
 
 % I1 + I2
-plot(Ch1Vdm21, Ch2I111+Ch2I121, 'r.')
-plot(Ch1Vdm22, Ch2I112+Ch2I122, 'g.')
-plot(Ch1Vdm23, Ch2I113+Ch2I123, 'b.')
+plot(Ch1Vdm21, Ch2I111+Ch2I121, '.', Color=black)
+plot(Ch1Vdm22, Ch2I112+Ch2I122, '.', Color=light_sea_green)
+plot(Ch1Vdm23, Ch2I113+Ch2I123, '.', Color=bittersweet)
 
 
+text(0,1.1e-5,'I_1+I_2')
+text(-0.5,0.1e-5,'I_1')
+text(0.5,0.1e-5,'I_2')
+text(-0.5,-0.9e-5,'I_1-I_2')
+
+ylim([-1.2e-5 1.2e-5])
 title("Differential Pair Characteristics Above Threshold (V_b = 0.8 V)")
-legend("4.5V", '3.5V', '2.5V', Location='southeast')
+lgd = legend("2.5V", '3.5V', '4.5V', Location='southeast');
+title(lgd,'V_2','FontSize',10)
 xlabel('Differential-Mode Input Voltage (V)')
 ylabel('Current (A)')
+%% 
 
-figure(2)
-hold on
-plot(Ch1Vdm31, Ch2V31, '.')
-plot(Ch1Vdm32, Ch2V32, '.')
-plot(Ch1Vdm33, Ch2V33, '.')
+clf;
+load data/exp1_SI.mat
 
-legend('4.5 V', '3.5 V', '2.5 V')
+hold on; grid minor
+plot(Ch1Vdm33, Ch2V33, '.', Color=black)
+plot(Ch1Vdm32, Ch2V32, '.', Color=light_sea_green)
+plot(Ch1Vdm31, Ch2V31, '.', Color=bittersweet)
+
+lgd = legend('2.5 V', '3.5 V', '4.5 V', Location='northwest')
+title(lgd,'V_2','FontSize',10)
 title('Common-Source Node Voltage Above Threshold (V_b = 0.8 V)')
 xlabel('Differential-Mode Input Voltage (V)')
 ylabel('Common-Source Node Voltage (V)')
+%% 
+clf;
+load data/exp1_SI.mat
+
+figure(1)
+hold on; grid minor
+
+[f1,l1,m1,b1,n1] = linefit(Ch1Vdm21, Ch2I111-Ch2I121, 5e-3);
+[f2,l2,m2,b2,n2] = linefit(Ch1Vdm22, Ch2I112-Ch2I122, 5e-3);
+[f3,l3,m3,b3,n3] = linefit(Ch1Vdm23, Ch2I113-Ch2I123, 5e-3);
+
+Gdm_25 = m3;
+Gdm_35 = m2;
+Gdm_45 = m1;
+
+% I1 - I2
+plot(Ch1Vdm23, Ch2I113-Ch2I123, '.', Color=black)
+plot(Ch1Vdm22, Ch2I112-Ch2I122, '.', Color=light_sea_green)
+plot(Ch1Vdm21, Ch2I111-Ch2I121, '.', Color=bittersweet)
+
+% Fit
+plot(Ch1Vdm23, (Ch1Vdm23)*m3+b3, '-', Color=black)
+plot(Ch1Vdm22, (Ch1Vdm22)*m2+b2, '-', Color=light_sea_green)
+plot(Ch1Vdm21, (Ch1Vdm21)*m1+b1, '-', Color=bittersweet)
+
+ylim([-1.5e-5 1.5e-5])
+
+legend('V_2=2.5 V', 'V_2=3.5 V', 'V_2=4.5 V', 'G_{dm}=6.0640e-05 mhos', ...
+    'G_{dm}=6.2231e-05 mhos', 'G_{dm}=6.4787e-05 mhos', Location="southeast");
+xlabel("Differential-Mode Input Voltage (V)")
+ylabel("I1-I2 (A)")
+
+title('Current Difference Above Threshold (V_b = 0.8 V)')
+xlabel('Differential-Mode Input Voltage (V)')
+ylabel('I_1-I_2 Current Difference (A)')
